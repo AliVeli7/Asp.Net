79650437137696fb68d3893194cac98cd3620d74
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-09 23:31:28 +0400

    3

--- WebUI/Models/Product.cs
+++ WebUI/Models/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace WebUI.Models
 {
     public class Product
-    {
+    {   
         public int Id { get; set; }
         public string Title { get; set; }
         public double Price { get; set; }
