f726063d75214835a7355e7d6abbf55b5368326d
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-10 00:43:15 +0400

    00:45

--- WebUI/Models/Slide.cs
+++ WebUI/Models/Slide.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace WebUI.Models
 {
     public class Slide
     {
         public string Url { get; set; }
+        [NotMapped, Required]
         public int Id { get; set; }
+        
+        public IFormFile Photo { get; set; }
     }
 }
