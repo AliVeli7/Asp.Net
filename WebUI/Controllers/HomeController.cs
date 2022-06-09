bc4312aa0f049e0d43e64dd10ac91e1044b41d49
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-09 22:45:47 +0400

    e

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.DAL;
+using WebUI.ViewModels;
 
 namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel home = new HomeViewModel
+            {
+                Slides = _context.Slides.ToList(),
+                Categories = _context.Categories.Where(c => !c.IsDeleted).ToList(),
+            };
+            return View(home);
         }
     }
 }
