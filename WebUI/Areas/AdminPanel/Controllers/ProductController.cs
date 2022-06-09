ea90c755be0f4d4dabc373f5f60195180629e4b8
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-10 01:17:53 +0400

    01:17

---
+++ WebUI/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.DAL;
+
+namespace WebUI.Areas.AdminPanel.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AppDbContext _context { get; }
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products);
+        }
+    }
+}
